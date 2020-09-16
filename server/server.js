e34fb117b7bfb158c5af2fdadbe57d3ebac57545
Pe3k <peto.fertal@gmail.com>
2020-09-16 10:04:43 +0200

    nastavnie prettier pri ukladani

--- server/server.js
+++ server/server.js
@@ -1,110 +1,99 @@
 const express = require("express");
 const app = express();
-const mariadb = require('mariadb');
-const bodyParser = require('body-parser')
+const mariadb = require("mariadb");
+const bodyParser = require("body-parser");
 app.use(express.json());
-const Joi = require('joi');
-require('dotenv').config();
+const Joi = require("joi");
+require("dotenv").config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 const pool = mariadb.createPool({
-    host: process.env.HOST, 
-    user: process.env.USER, 
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
-
 
 // --------------------     GET   --------------------------
 
-app.get('/data', (req, res) => {
-    return pool.query('SELECT * FROM ulohy')
-    .then(result => {
-        res.json(result);
-    });
+app.get("/data", (req, res) => {
+  return pool.query("SELECT * FROM ulohy").then((result) => {
+    res.json(result);
+  });
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello');
-  });
-
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 // --------------------     POST   --------------------------
 
-app.use(bodyParser.json())
-app.post('/', (req, res) => {
-    
-    pool.getConnection()
-    .then(conn => {
-        if(req.body.newData === "") {console.log("Prazdne pole")
+app.use(bodyParser.json());
+app.post("/", (req, res) => {
+  pool.getConnection().then((conn) => {
+    if (req.body.newData === "") {
+      console.log("Prazdne pole");
     } else {
-
-                conn.query(`INSERT INTO ulohy (title) VALUE ("${req.body.newData}")`
-                )}
-            })  
+      conn.query(`INSERT INTO ulohy (title) VALUE ("${req.body.newData}")`);
+    }
+  });
 });
-
 
 // ------------------------  UPDATE  ----------------------------
 
-app.post('/update/:id', (req, res) => {
-    pool.getConnection()
-    .then(conn => {
-        if(req.body.newId === "" || req.body.newData === '') {console.log("Prazdne pole")
-} else {
-        conn.query(`UPDATE ulohy SET title = '${req.body.newData}' WHERE id = ${req.body.newId}`)
+app.post("/update/:id", (req, res) => {
+  pool.getConnection().then((conn) => {
+    if (req.body.newId === "" || req.body.newData === "") {
+      console.log("Prazdne pole");
+    } else {
+      conn.query(
+        `UPDATE ulohy SET title = '${req.body.newData}' WHERE id = ${req.body.newId}`
+      );
     }
-})
+  });
 });
 
 // ------------------------  DELETE  ----------------------------
 
-app.post('/delete/:id', (req, res) => {
-    pool.getConnection()
-        .then(conn => {
-            if(req.body.newId === "") {console.log("Prazdne pole")
+app.post("/delete/:id", (req, res) => {
+  pool.getConnection().then((conn) => {
+    if (req.body.newId === "") {
+      console.log("Prazdne pole");
     } else {
-            conn.query(`DELETE FROM ulohy WHERE id = "${req.body.newId}"`)
-        }
-    })
+      conn.query(`DELETE FROM ulohy WHERE id = "${req.body.newId}"`);
+    }
+  });
 });
-
 
 //   -----------------------------SEARCH   ----------------------------
 
-app.post('/search', (req, res) => {
-    
-    pool.getConnection()
-    .then(conn => {
-        if(req.body.newData === "") {console.log("Prazdne pole")
+app.post("/search", (req, res) => {
+  pool.getConnection().then((conn) => {
+    if (req.body.newData === "") {
+      console.log("Prazdne pole");
     } else {
-
-                conn.query(`SELECT * FROM ulohy WHERE title = '${req.body.newData}'`
-                )
-                .then(result => {
-                    res.json(result);
-                    // console.log(result)
-                });}
-           
-            })  
+      conn
+        .query(`SELECT * FROM ulohy WHERE title = '${req.body.newData}'`)
+        .then((result) => {
+          res.json(result);
+          // console.log(result)
+        });
+    }
+  });
 });
-
 
 //  -----------------------  VALIDACIA  ------------------------
 
-
 function validateTask(task) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
 
-    return Joi.validate(task, schema);
+  return Joi.validate(task, schema);
 }
 
-
-
 // console.log(localDatabase)
-app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`));
-
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}...`)
+);
--- src/table.jsx
+++ src/table.jsx
@@ -5,9 +5,7 @@ import { DownloadOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
 import { Form, Input } from "antd";
 
-
 export default function MyTable(props) {
-
   const columns = [
     {
       title: "Id",
@@ -16,40 +14,55 @@ export default function MyTable(props) {
     {
       title: "Task Title",
       dataIndex: "title",
-    }
+    },
   ];
 
   return (
     <div>
-  <Table columns={columns} rowKey='id' dataSource={props.tableData} className="table"/>
+      <Table
+        columns={columns}
+        rowKey="id"
+        dataSource={props.tableData}
+        className="table"
+      />
       <div className="navigation">
-        <Form  style={{textAlign:'center'}} >
-        <Button
-          type="primary"
-          icon={<DownloadOutlined />}
-          size="normal"
-          onClick={() => props.onChangeGetData()}
-        >
-          Get Task
-        </Button>
-        <Button id="putBtn" onClick={() => props.onChangeSearchData()}>
-          Search Task
-        </Button>
+        <Form style={{ textAlign: "center" }}>
+          <Button
+            type="primary"
+            icon={<DownloadOutlined />}
+            size="normal"
+            onClick={() => props.onChangeGetData()}
+          >
+            Get Task
+          </Button>
+          <Button id="putBtn" onClick={() => props.onChangeSearchData()}>
+            Search Task
+          </Button>
           <Form.Item label="Task" name="task">
-            <Input name="name" id="dataName" value={props.newData} onChange={(event) => props.onChangeTask(event)} />
+            <Input
+              name="name"
+              id="dataName"
+              value={props.newData}
+              onChange={(event) => props.onChangeTask(event)}
+            />
           </Form.Item>
           <Form.Item label="Id" name="taskId">
-          <Input name="id" id="dataId" value={props.newId} onChange={(event) => props.onChangeId(event)} />
+            <Input
+              name="id"
+              id="dataId"
+              value={props.newId}
+              onChange={(event) => props.onChangeId(event)}
+            />
           </Form.Item>
-        
+
           <Button type="primary" onClick={() => props.onChangePostData()}>
-           Add Task
+            Add Task
           </Button>
           <Button id="putBtn" onClick={() => props.onChangeUpdateData()}>
-          Update Task
-        </Button>
+            Update Task
+          </Button>
           <Button danger onClick={() => props.onChangeDeleteData()}>
-           Delete Task
+            Delete Task
           </Button>
         </Form>
         {/* {props.alertMessage} */}
